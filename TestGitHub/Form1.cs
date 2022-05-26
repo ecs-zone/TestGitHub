8058371bc32022d01b61e39371586f13b0f9e699
ecs-zone <91863668+ecs-zone@users.noreply.github.com>
2022-05-26 15:50:00 +0700

    .1

--- TestGitHub/Form1.cs
+++ TestGitHub/Form1.cs
@@ -19,7 +19,8 @@ namespace TestGitHub
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hi Siriwat");
+            MessageBox.Show("Hi Siriwat 1");
+            MessageBox.Show("Hi Zone 1");
         }
     }
 }
