02fac79ad7fc3a739066dcf00a453187e23302c4
siriwat.zone <siriwat.zone@gmail.com>
2023-07-12 11:55:59 +0700

    remove

--- TestGitHub/Form1.cs
+++ TestGitHub/Form1.cs
@@ -21,11 +21,6 @@ namespace TestGitHub
         {
             MessageBox.Show("Hi Siriwat 122");
             string xs = "Master to fix";
-
-            //Add master
-            //Add hot fix
-            //Add hot fix 02
-            //Add hot fix 03
         }
     }
 }
