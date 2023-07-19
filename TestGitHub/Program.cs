771f4affcd54a34e726249c1cd8017d2ddbdb8e4
siriwat.zone <siriwat.zone@gmail.com>
2023-07-19 12:01:27 +0700

    add 1

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -17,7 +17,7 @@ namespace TestGitHub
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
+            //XXXXXXXXX
         }
     }
 }
